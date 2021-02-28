112ab8fbfc4a94ab47bc31d830de6d87f7cd6a9b
VARAGANTITANVI <75781381+VARAGANTITANVI@users.noreply.github.com>
2021-02-28 18:00:55 +0530

    Add files via upload

---
+++ ingridients.js
@@ -0,0 +1,3 @@
+noodeles
+water 
+noodeles masala 
---
+++ recipe.js
@@ -0,0 +1,8 @@
+heat the water for 10 or 15 mins  on low flame.
+till the water gets heated up cut some vegetables like carrot onion and banata to add it in the noodles.
+when the water is heated up then add the noodles into the hot water .
+after 10 to 20 mins when the noodles is boiled 
+add the chopped vegetables in the noodles.
+add the masala into it.
+then your favorite maggi noodles is ready. 
+serve it in a bowl and enjoy it.
